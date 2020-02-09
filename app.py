07c11d844b1f6d53d97224669cf77f013315dda6
Stella Wander <jiayichenjudy@gmail.com>
2020-02-09 13:11:42 -0500

    added tweet

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 import os
 from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
-
-
+# make sure to use the up-to-date import formet: from flast_module import Module
+# DO NOT use deprecated from flask.ext.module import Module
+import tweepy
 
 app = Flask(__name__)
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config.setdefault('TWEEPY_CONSUMER_KEY', 'R4M3HyEF5FcDbdd0s3Xyohdeq')
+# app.config.setdefault('TWEEPY_CONSUMER_SECRET', 'KpWOI1udl9gXDJow3mGx9yK9OPncqZ0PvMs8whdWfHEyEImPCy')
+# app.config.setdefault('TWEEPY_ACCESS_TOKEN_KEY', '1198415155752849409-XLDpSZ3irzZQGhP1CUKs27Xgfey1Me')
+# app.config.setdefault('TWEEPY_ACCESS_TOKEN_SECRET', 'tH2lp62ZgqSC4KE70VXJ6vSfHuyudXS3Br7uKTOcG5Dj9')
+# tweepy = Tweepy(app)
+
+auth = tweepy.OAuthHandler('R4M3HyEF5FcDbdd0s3Xyohdeq', 'KpWOI1udl9gXDJow3mGx9yK9OPncqZ0PvMs8whdWfHEyEImPCy')
+auth.set_access_token('1198415155752849409-XLDpSZ3irzZQGhP1CUKs27Xgfey1Me', 'tH2lp62ZgqSC4KE70VXJ6vSfHuyudXS3Br7uKTOcG5Dj9')
+
+api = tweepy.API(auth)
+
 db = SQLAlchemy(app)
+print("tweepy")
+print(tweepy)
 
 class Topic(db.Model):
     __tablename__ = 'topics'
@@ -32,6 +46,17 @@ class Topic(db.Model):
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route('/tweets')
+def show_tweets():
+    timeline = api.home_timeline()
+    tweets = []
+    for t in timeline:
+        tweets.append(t.text)
+    topicNames = ["no topics"]
+    info = "no infotext"
+    return render_template("getdata.html", topicNames = topicNames, infotext = info, tweets = tweets)
+
 
 @app.route("/add")
 def add_topic():
